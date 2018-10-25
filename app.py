cf4f60b15db07dfd76a650f646a865da9a54d004
georgea18-meet <abudauodg@gmail.com>
2018-10-25 18:46:23 +0300

    shop and home

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import *
+
 app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return None
+    return render_template("home.html")
+
+@app.route('/shop')
+def shop_page():
+    products=["pdp", "vrm", "pcm"]
+    return render_template("shop.html",
+        products=products)
 
 if __name__ == '__main__':
    app.run(debug = True)
